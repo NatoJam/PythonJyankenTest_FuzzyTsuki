ff1edf9495a1b0ea0000c976fddde429225f0a00
Naoto <116436233+NatoJam@users.noreply.github.com>
2023-10-18 14:10:07 +0900

    id added

--- tst1.py
+++ tst1.py
@@ -2,5 +2,8 @@ import random
 def print_halo():
     name = input("Please input your name:\n")
     print("Hello~, "+name+".\n")
-
+    sum = 0
+    for i in name:
+        sum += ord(i)
+    print("Your identification number is: "+str(sum)+".\n")
 print_halo()
